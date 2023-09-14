8558a4f96d0e0cf2d8cece7018ccc58c8b3c5fe1
Replit user <>
2023-09-14 18:32:17 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -168,34 +168,24 @@ void eraseTreeMap(TreeMap * tree, void* key){
 
 }
 
-Pair* searchTreeMapRecursive(TreeNode* currentNode, void* key, int (*lower_than)(void*, void*), TreeNode** foundNode) {
-    if (currentNode == NULL) {
-        *foundNode = NULL;
-        return NULL;
-    }
-
-    int cmp = lower_than(key, currentNode->pair->key);
-
-    if (cmp == 0) {
-      *foundNode = currentNode;
-      return currentNode->pair;
-      
-    } else if (cmp < 0) {
-        return searchTreeMapRecursive(currentNode->left, key, lower_than, foundNode);
-    } else {
-        return searchTreeMapRecursive(currentNode->right, key, lower_than, foundNode);
-    }
-}
-
 Pair* searchTreeMap(TreeMap* tree, void* key) {
-    TreeNode* foundNode = NULL;
-    Pair* result = searchTreeMapRecursive(tree->root, key, tree->lower_than, &foundNode);
+    TreeNode* currentNode = tree->root;
 
-    tree->current = foundNode;
+    while (currentNode != NULL) {
+        int equal = is_equal(tree, key, currentNode->pair->key);
 
-    return result;
+        if (equal) {
+            tree->current = currentNode;
+            return currentNode->pair;
+        } else if (tree->lower_than(key, currentNode->pair->key) < 0) {
+            currentNode = currentNode->left;
+        } else {
+            currentNode = currentNode->right;
+        }
+    }
+    tree->current = NULL;
+    return NULL;
 }
-
 
 
 
