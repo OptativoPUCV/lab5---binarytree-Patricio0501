6ef5ecbbbd199156d08f4cb452d9e0cd334e0649
Replit user <>
2023-09-14 14:46:34 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -126,7 +126,7 @@ Pair* searchTreeMap(TreeMap* tree, void* key) {
         if (cmp == 0) {
             tree->current = current;
             return current->pair;
-        } else if (cmp < 0) {
+        } else if (cmp > 0) {
             current = current->left;
         } else {
             current = current->right;
